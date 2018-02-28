0846e2ce8f30320503d1152ec28a44ab31217668
axel <111501034@smail.iitpkd.ac.in>
2018-02-28 08:47:26 +0530

    check_seat_availability

--- procedures.sql
+++ procedures.sql
@@ -4,6 +4,7 @@ CREATE PROCEDURE new_user_view (IN ID int,OUT param1 INT)
  END;
 //
 
-CREATE FUNCTION check_seat_available ( CHAR(20))
-    RETURNS CHAR(50) DETERMINISTIC
-    RETURN CONCAT('Hello, ',s,'!');
+CREATE FUNCTION check_seat_available (FID int,cl enum('Business','FirstClass','Economy'))
+    RETURNS INT
+    RETURN (SELECT COUNT(*) FROM Seats WHERE ID NOT IN (SELECT SeatID FROM Tickets WHERE FlightID=FID) and class=cl and planeId=(SELECT PlaneID FROM ScheduledFlights WHERE ID=FID)); 
+
